c72fb7baf679115d6fac3f50af8b5685fcb08d22
bruce <bruce@brucecooper.net>
2022-06-19 14:40:46 +0800

    Changed read approach to use TCB0

--- dali.c
+++ dali.c
@@ -152,36 +152,52 @@ dali_result_t dali_transmit_cmd(uint8_t addr, uint8_t cmd) {
  */
 
 typedef enum {
-    HALF_BIT,
-    FULL_BIT,
-    INVALID_TOO_SHORT,
-    INVALID_MIDDLE,
-    INVALID_TOO_LONG,
+    PULSE_TIMING,
+    PULSE_HALF_BIT,
+    PULSE_FULL_BIT,
+    PULSE_INVALID_TOO_SHORT,
+    PULSE_INVALID_MIDDLE,
+    PULSE_INVALID_TOO_LONG,
 } pulsewidth_t;
 
+static volatile pulsewidth_t current_pulse_width = PULSE_TIMING;
 
-pulsewidth_t wait_for_level_change(uint8_t *current_val) {
-    uint8_t new_val;
 
-    while (TCA0.SINGLE.CNT <= FULLTICK_MAX_TICKS) {
-        new_val = DALI_PORT.IN & DALI_RX_bm;
-        if (new_val != *current_val) {
-            uint16_t pulse_length = TCA0.SINGLE.CNT;
-            TCA0.SINGLE.CNT = 0; // Reset the clock to time the next transition.  TODO give it a head start, for the additional instructions.
-            *current_val = new_val;
-
-            if (pulse_length < HALFTICK_MAX_TICKS) {
-                return INVALID_TOO_SHORT;
-            } else if (pulse_length <= HALFTICK_MAX_TICKS) {
-                return HALF_BIT;
-            } else if (pulse_length <= FULLTICK_MIN_TICKS) {
-                return INVALID_TOO_SHORT;
-            } else if (pulse_length <= FULLTICK_MAX_TICKS) {
-                return FULL_BIT;
-            }
-        }
+ISR(TCB0_INT_vect)
+{
+    uint16_t pulse_length = TCB0.CCMP;  // This will clear the interrupt flag.
+    
+    if (pulse_length < HALFTICK_MAX_TICKS) {
+        current_pulse_width = PULSE_INVALID_TOO_SHORT;
+    } else if (pulse_length <= HALFTICK_MAX_TICKS) {
+        current_pulse_width = PULSE_HALF_BIT;
+    } else if (pulse_length <= FULLTICK_MIN_TICKS) {
+        current_pulse_width = PULSE_INVALID_TOO_SHORT;
+    } else if (pulse_length <= FULLTICK_MAX_TICKS) {
+        current_pulse_width = PULSE_FULL_BIT;
+    } else {
+        current_pulse_width = PULSE_INVALID_TOO_LONG;
     }
-    return INVALID_TOO_LONG;
+
+    // Flip the edge type we're looking for.
+    TCB0.EVCTRL ^= TCB_EDGE_bm; // 1 is negative edge, 0 is positive edge
+}
+
+
+pulsewidth_t time_pulse(uint8_t *current_val) {
+    // Use TCB in Input Capture Frequency Measurement mode.  This will generate a CAPT interrupt on edge transition, and copy
+    // the clock value into CCMP.  This will result in accurate pulse width measurement.
+    // How to deal with a pulse that goes on for a long high pulse (like a stop condition) though?
+    // Have the main loop check to see if CNT goes above the too long threhsold.  When it does, disable the counter and proceed as if TOO_LONG
+
+    
+    // is there a race condition here on compare to CNT?  If there is, it'll just loop again, and you'll be fine.
+    for (current_pulse_width = PULSE_TIMING; current_pulse_width == PULSE_TIMING && TCB0.CNT < FULLTICK_MAX_TICKS; ) { 
+        // We're just waiting, so do nothing.
+    }
+    pulsewidth_t result = current_pulse_width == PULSE_TIMING ? PULSE_INVALID_TOO_LONG : current_pulse_width;
+    current_pulse_width = PULSE_TIMING;
+    return result;
 }
 
 
@@ -192,43 +208,44 @@ dali_result_t dali_receive(uint8_t *address, uint8_t *command) {
     uint16_t shiftreg = 0;
     pulsewidth_t pulse_width;
 
-    // If the bus is high, then its not the start of a packet.
+    // Check to see if the line has been pulled down (indicating the start of a transmission)
     if (DALI_PORT.IN & DALI_RX_bm) {
         return DALI_NO_START_BIT;
     }
-    // TODO disable interrupts?
 
-    // Okay, sombebody has pulled the line down, which is hopefully the start of a transmission. Set up the timer.
-    TCA0.SINGLE.CNT = 0; // start at zero.
-    TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV1_gc | TCA_SINGLE_ENABLE_bm; // Enable the timer.
-    TCA0.SINGLE.PER = 65535; // max value.
-    TCA0.SINGLE.INTCTRL = 0; // no interrupts.  We're just using the timer to see how long it takes.
+    // We know we're low at this point, which is hopefully the start of a transmission. Set up the timer.
+    TCB0.CNT = 3;
+    TCB0.CTRLB = TCB_CNTMODE_FRQ_gc; // Put it in Capture Frequency Measurement mode.
+    TCB0.EVCTRL = TCB_CAPTEI_bm; // Waiting for a positive edge to start with.
+    TCB0.CTRLA = TCB_CLKSEL_CLKDIV1_gc | TCB_ENABLE_bm;
 
-    pulse_width = wait_for_level_change(&current_val);
-    if (pulse_width != HALF_BIT) {
+
+    // Wait for first half bit of the start bit, which must be low for one half bit period.
+    pulse_width = time_pulse(&current_val);
+    if (pulse_width != PULSE_HALF_BIT) {
         goto cleanup;
     }
 
     // Now we're half way through the start bit.
     while (res != DALI_OK) {
         // At the top of the loop we are always a half bit period into a bit.
-        pulse_width = wait_for_level_change(&current_val);
+        pulse_width = time_pulse(&current_val);
 
-        if (pulse_width == HALF_BIT) {
+        if (pulse_width == PULSE_HALF_BIT) {
              // There must be a followup half tick sized pulse (unless line has been high for a while, and the last bit was a zero, indicating an end of transmission)
-             pulse_width = wait_for_level_change(&current_val);
+             pulse_width = time_pulse(&current_val);
 
-             if (pulse_width == HALF_BIT) {
+             if (pulse_width == PULSE_HALF_BIT) {
                 shiftreg = (shiftreg << 1) | last_bit;    
-             } else if (pulse_width == INVALID_TOO_LONG && current_val > 0 && last_bit == 0) {
+             } else if (pulse_width == PULSE_INVALID_TOO_LONG && current_val > 0 && last_bit == 0) {
                 // that was the end of the packet.
                 res = DALI_OK;
              }
-        } else if (pulse_width == FULL_BIT) {
+        } else if (pulse_width == PULSE_FULL_BIT) {
             // Its a bit toggle, and we remain in the half bit position.
             last_bit = last_bit ? 0 : 1;
             shiftreg = (shiftreg << 1) | last_bit;
-        } else if (pulse_width == INVALID_TOO_LONG && current_val > 0 && last_bit == 1) {
+        } else if (pulse_width == PULSE_INVALID_TOO_LONG && current_val > 0 && last_bit == 1) {
             // The end of the packet.
             res = DALI_OK;
         } else {
@@ -237,10 +254,14 @@ dali_result_t dali_receive(uint8_t *address, uint8_t *command) {
         }
     } 
 
-    // We waited for the line to be high for 1 bit period, but we now need to wait for one more
-    
-
+    // If we get here, we've already waited for the line to be high for 1 bit period, but we now need to wait for one more
 cleanup:
-    TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV1_gc; // disable the timer.
+    TCB0.CTRLA = 0; // Disable TCB0
+    // wait for line to be high for at least x ms
+    // step 1 - wait for it to be high.
+    while ((DALI_PORT.IN & DALI_RX_bm) == 0) {
+        // Do nothing.
+    }
+    _delay_ms(1); // Poor mans way of waiting for line...
     return res;
 }
