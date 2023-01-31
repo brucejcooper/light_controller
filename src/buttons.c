0c24f1521164e0b5035d1bbd4ef66afe3aabd795
bruce <bruce@brucecooper.net>
2023-02-01 04:36:20 +0800

    Just going silly on the flash optimisation now..  Went back to enum based states as it uses lots less instructions than function pointer based

--- src/buttons.c
+++ src/buttons.c
@@ -12,112 +12,114 @@
 #include <stdlib.h>
 #include "cmd.h"
 
-// Switch is on PB2
+// For test board, switch is on PA6
 #define SWITCH_PORT PORTA
+#define MS_TO_RTC_TICKS(m) (m * 32768 / 1000 / 2)
 struct button_t;
 
 typedef enum {
     BTN_STATE_RELEASED,
-    // BTN_STATE_DEBOUNCING,
     BTN_STATE_PRESSED,
     BTN_STATE_RELEASED_DEBOUNCING,
     BTN_STATE_LONGHELD,
 } button_state_t; 
 
+
+typedef void (*state_handler_t)(struct button_t *btn, const uint8_t button_level);
 typedef struct button_t {
+    // Index of the button.
+    uint8_t index;
+    // Counts how many times the button has been clicked to switch fade direction
     uint8_t click_count;
-    uint16_t next_action;
+    // RTC timeout for next 
+    uint16_t timeout;
+    // Bit mask for the pin
     uint8_t mask;
+    // The state handler.
     button_state_t state;
-    uint8_t level;
+    // The last read light level for this target.
+    uint8_t light_level;
 } button_t;
 
+static void released(button_t *btn, const uint8_t button_level);
+static void pressed(button_t *btn, const uint8_t button_level);
+static void long_pressed(button_t *btn, const uint8_t button_level);
+static void released_debouncing(button_t *btn, const uint8_t button_level);
+static void long_pressed(button_t *btn, const uint8_t button_level);
 
-/**
- * @brief Timeout for when next action will happen 
- * if positive, this is the number of ticks until the next action for that button
- * if negative, then timer is disabled for that button.
- */
-static button_t buttons[NUM_BUTTONS] = {
+static const button_t buttons[NUM_BUTTONS] = {
     {
         .state = BTN_STATE_RELEASED,
         .mask = PIN6_bm,
+        .light_level = 0,
+        .click_count = 0,
+        .timeout = 0,
     }
 };
 
 void buttons_init() {
     // Set up pins with pullup, and interrupt on 
     SWITCH_PORT.PIN6CTRL = PORT_PULLUPEN_bm | PORT_ISC_LEVEL_gc;
-
-    for (int i = 0; i < NUM_BUTTONS; i++) {
-        SWITCH_PORT.DIRCLR = buttons[i].mask; // Set all buttons as inputs.
-        buttons[i].state = BTN_STATE_RELEASED;  // Start in released state -It will immediately go into button press if it is already held down.
-    }
+    // Set all the pins as inputs.
+    SWITCH_PORT.DIRCLR = PIN6_bm;
 }
 
 static inline bool is_timer_expired(button_t *btn) {
-    return ((int16_t) (RTC.CNT - (btn->next_action))) > 0;
+    return ((int16_t) (RTC.CNT - btn->timeout)) >= 0;
 }
 
 
+static void released(button_t *btn, const uint8_t button_level) {
+    if (!button_level) {
+        // Its been pressed.
+        btn->state = BTN_STATE_PRESSED;
+        btn->timeout = RTC.CNT + config->doublePressTimer;
 
-static void poll_button(uint8_t index, button_t *btn) {
-    uint8_t val = SWITCH_PORT.IN & btn->mask;
-    read_result_t res;
+        // Whilst we're waiting for the button to debounce, ask the ballast its current level.
+        // Because this takes some time (a little over 20 ms, post response delay), there is no 
+        // need for an additional debounce timer.
+        read_result_t res = send_dali_cmd(config->targets[btn->index], DALI_CMD_QUERY_ACTUAL_LEVEL, &btn->light_level);            
+        if (res != READ_VALUE) {
+            log_info("Did not receive response from ballast");
+            btn->light_level = 0; // default to off, meaning when we release the light should be turned on.
+        }
+    }
+}
+
+static void pressed(button_t *btn, const uint8_t button_level) {
     uint8_t dummy;
-
-    switch (btn->state) {
-        case BTN_STATE_RELEASED:
-        if (val == 0) {
-            // Its been pressed.
-            btn->state = BTN_STATE_PRESSED;
-            btn->next_action = RTC.CNT + config->doublePressTimer;
-
-            // Whilst we're waiting for the button to debounce, ask the ballast its level
-            res = send_dali_cmd(config->targets[index], DALI_CMD_QUERY_ACTUAL_LEVEL, &btn->level);            
-            if (res != READ_VALUE) {
-                log_info("Did not receive response from ballast");
-                btn->level = 0; // default to off, meaning when we release the light should be turned on.
-            }
+    if (button_level) {
+        // Its been released - send out either an off or an on command, depending ont he current level
+        log_uint8("Released", btn->index);
+        log_uint8("Old value ", btn->light_level);
+        read_result_t res = send_dali_cmd(config->targets[btn->index], btn->light_level ? DALI_CMD_OFF : DALI_CMD_GO_TO_LAST_ACTIVE_LEVEL, &dummy);
+        if (res != READ_NAK) {
+            log_info("Received unexpected response from command");
         }
-        break;
+        // The command will have taken a little over 10 milliseconds.  This is effectively our debounce period.
+        btn->state = BTN_STATE_RELEASED;
+    } else if (is_timer_expired(btn)) {
+        log_info("Long pressed");
+        btn->state = BTN_STATE_LONGHELD;
+        btn->timeout = RTC.CNT + config->repeatTimer; 
+    }
+}
 
-        case BTN_STATE_RELEASED_DEBOUNCING:
-        if (is_timer_expired(btn)) {
-            btn->state = BTN_STATE_RELEASED;
-        }
-        break;
+static void long_pressed(button_t *btn, const uint8_t button_level) {
+    if (button_level) {
+        // It was released. Do some debouncing.
+        btn->state = BTN_STATE_RELEASED_DEBOUNCING;
+        btn->timeout = RTC.CNT + MS_TO_RTC_TICKS(10);
+    } else if (is_timer_expired(btn)) {
+        // Its been held long enough now for a repeat.
+        btn->timeout = RTC.CNT + config->repeatTimer; 
+        log_uint8("Repeat", btn->index);
+    }
+}
 
-        case BTN_STATE_PRESSED:
-        if (val) {
-            log_uint8("Released", index);
-            log_uint8("Old value ", btn->level);
-            res = send_dali_cmd(config->targets[index], btn->level ? DALI_CMD_OFF : DALI_CMD_GO_TO_LAST_ACTIVE_LEVEL, &dummy);
-            if (res != READ_NAK) {
-                log_info("Received unexpected response from command");
-            }
-        }
-
-        // Fall through into long_hold - when the timer expires it will start sending long_held messages
-
-
-        case BTN_STATE_LONGHELD:
-        if (val) {
-            // It was released.
-            btn->state = BTN_STATE_RELEASED_DEBOUNCING;
-            btn->next_action = RTC.CNT + config->repeatTimer;
-
-        } else if (is_timer_expired(btn)) {
-            // Its been held long enough now
-            btn->next_action = RTC.CNT + config->repeatTimer; 
-            if (btn->state == BTN_STATE_PRESSED) {
-                log_uint8("Long Held", index);
-                btn->state = BTN_STATE_LONGHELD;
-            } else {
-                log_uint8("Repeat", index);
-            }
-        }
-        break;
+static void released_debouncing(button_t *btn, const uint8_t button_level) {
+    if (is_timer_expired(btn)) {
+        btn->state = BTN_STATE_RELEASED;
     }
 }
 
@@ -125,10 +127,24 @@ static void poll_button(uint8_t index, button_t *btn) {
 bool poll_buttons() {
     bool all_idle = true;
 
-    for (uint8_t b = 0; b < NUM_BUTTONS; b++) {
-        poll_button(b, &buttons[b]);
-        // If any button is doing anything other than in the released state, then we stay awake.
-        if (buttons[b].state != BTN_STATE_RELEASED) {
+    for  (button_t *btn = (button_t *) buttons; btn < (buttons+NUM_BUTTONS); btn++) {
+        uint8_t val = SWITCH_PORT.IN & btn->mask;
+
+        switch (btn->state) {
+            case BTN_STATE_RELEASED:
+            released(btn, val);
+            break;
+            case BTN_STATE_PRESSED:
+            pressed(btn, val);
+            break;
+            case BTN_STATE_LONGHELD:
+            long_pressed(btn, val);
+            break;
+            case BTN_STATE_RELEASED_DEBOUNCING:
+            released_debouncing(btn, val);
+            break;
+        }
+        if (btn->state != BTN_STATE_RELEASED) {
             all_idle = false;
         }
     }
--- src/buttons.h
+++ src/buttons.h
@@ -1,21 +1,7 @@
 #ifndef __BUTTONS_H__
 #define __BUTTONS_H__
 
-
-typedef enum {
-  EVENT_NONE,
-  EVENT_PRESSED,
-  EVENT_LONG_PRESSED,
-  EVENT_RELEASED,
-} button_event_type_t;
-
 #define NUM_BUTTONS (1)
-
-typedef struct {
-  button_event_type_t type;
-  uint8_t index;
-} button_event_t;
-
 
 extern void buttons_init();
 bool poll_buttons();
